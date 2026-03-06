a49d5429c9f2397957bfc555bf7910dc78c2adbd
Vansh-Arora01 <vansharora0112@gmail.com>
2026-03-06 15:01:48 +0530

    Change for app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-});
-
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Smart Complaint Management System',
+        short_name: 'SCMS',
+        description: 'Complaint Management System for Students',
+        theme_color: '#2563eb',
+        icons: [
+          {
+            src: 'SCMS_logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'SCMS_logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
