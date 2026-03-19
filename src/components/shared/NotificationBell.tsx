8cd103e330e8d7d2aaac7e43b93aeb04d6fba3c9
Vansh-Arora01 <vansharora0112@gmail.com>
2026-03-19 23:38:06 +0530

    add noti 4

--- src/components/shared/NotificationBell.tsx
+++ src/components/shared/NotificationBell.tsx
@@ -2,6 +2,7 @@ import { Bell } from "lucide-react";
 import { useState, useEffect } from "react";
 import NotificationPanel from "./NotificationPanel";
 import { notificationService } from "../../services/notification.service";
+import { createPortal } from "react-dom";
 
 export default function NotificationBell() {
   const [open, setOpen] = useState(false);
@@ -24,31 +25,42 @@ export default function NotificationBell() {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+  const handleClickOutside = (e: any) => {
+    if (!e.target.closest(".notification-container")) {
+      setOpen(false);
+    }
+  };
+
+  document.addEventListener("mousedown", handleClickOutside);
+  return () => document.removeEventListener("mousedown", handleClickOutside);
+}, []);
   return (
-    <div className="relative">
+  <div className="relative notification-container">
 
-      {/* 🔔 Bell */}
-      <button
-        onClick={() => setOpen(!open)}
-        className="relative p-2 rounded-lg hover:bg-slate-800"
-      >
-        <Bell className="text-white" />
+    {/* 🔔 Bell */}
+    <button
+      onClick={() => setOpen(!open)}
+      className="relative p-2 rounded-lg hover:bg-slate-800"
+    >
+      <Bell className="text-white" />
 
-        {/* 🔴 Badge */}
-        {count > 0 && (
-          <span className="absolute -top-1 -right-1 bg-red-500 text-xs px-1 rounded-full">
-            {count}
-          </span>
-        )}
-      </button>
+      {count > 0 && (
+        <span className="absolute -top-1 -right-1 bg-red-500 text-xs px-1 rounded-full">
+          {count}
+        </span>
+      )}
+    </button>
 
-      {/* Dropdown */}
-      {open && (
-  <div className="fixed top-20 right-6 w-[350px] bg-slate-900 border border-slate-700 rounded-xl shadow-lg p-4 z-[9999] max-h-[400px] overflow-y-auto">
-    <NotificationPanel />
+    {/* 🔥 Portal Dropdown */}
+    {open &&
+      createPortal(
+        <div className="fixed top-20 right-6 w-[350px] bg-slate-900 border border-slate-700 rounded-xl shadow-lg p-4 z-[9999] max-h-[400px] overflow-y-auto notification-container">
+          <NotificationPanel />
+        </div>,
+        document.getElementById("portal-root")!
+      )}
+
   </div>
-)}
-
-    </div>
-  );
+);
 }
