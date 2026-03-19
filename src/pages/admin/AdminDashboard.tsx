97fb31bf04a515921580bec719e8bc7a369234ea
Vansh-Arora01 <vansharora0112@gmail.com>
2026-03-19 23:26:43 +0530

    Add noti3

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -341,7 +341,7 @@ const AdminDashboard = () => {
         initial={{ opacity: 0, x: 30 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.4 }}
-        className="flex-1 flex flex-col relative z-0 min-w-0 w-full overflow-y-auto"
+        className="flex-1 flex flex-col relative z-0 min-w-0 w-full overflow-visible"
 
       >
 
